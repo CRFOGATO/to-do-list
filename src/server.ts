8ed8abf6ba59886345edab1b607c0ad7f30edc84
crfogato <crfogato@gmail.com>
2025-06-10 20:28:28 -0300

    'add-rotas'

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,24 @@
 import express from "express";
 import dotenv from "dotenv";
 import path from "path";
-import mainRoutes from "./routes/index";
+import cors from "cors";
+import apiRoutes from "./routes/api";
+import { error } from "console";
 
 dotenv.config();
 
 const server = express();
 
-server.use(express.static(path.join(__dirname, "../public")));
+server.use(cors());
 
-server.use(mainRoutes);
+server.use(express.static(path.join(__dirname, "../public")));
+server.use(express.urlencoded({ extended: true }));
+
+server.use(apiRoutes);
 
 server.use((req, res) => {
-  res.send("Pagina não encontrada");
+  res.status(404);
+  res.json({ error: "Pagina não encontrada" });
 });
 
 server.listen(process.env.PORT);
